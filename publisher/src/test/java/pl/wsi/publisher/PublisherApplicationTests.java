3cb17985c6bc1a441f8d8ca7e9876ad5f92a234a
wsi <skubala.wojciech@gmail.com>
2024-02-04 10:38:25 +0100

    Initialize publisher

---
+++ publisher/src/test/java/pl/wsi/publisher/PublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wsi.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
